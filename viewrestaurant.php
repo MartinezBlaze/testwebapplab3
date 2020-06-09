3630dd146c28065a54c729e9a5745d9c7451a6f4
Yoseph Martin Christian <14261062+MartinezBlaze@users.noreply.github.com>
2020-06-09 11:31:23 +0700

    Update viewrestaurant.php

--- viewrestaurant.php
+++ viewrestaurant.php
@@ -1,3 +1,6 @@
-<?php        $serverName = "testdbtp044890.database.windows.net";         $connectionOptions = array(                             "Database" => "testdbtp044890",                             "Uid" => "apuadmin",                             "PWD" => "Martin070899?");                          //Establishes the connection        $conn = sqlsrv_connect($serverName, $connectionOptions);         if (!$conn) {            die("Error connection: ".sqlsrv_errors());        }
+<?php        $serverName = "testdbtp044890.database.windows.net";         $connectionOptions = array(                             "Database" => "testdbtp044890",                             "Uid" => "apuadmin",                             "PWD" => "Martin070899?");                          
+
+//Establishes the connection        
+$conn = sqlsrv_connect($serverName, $connectionOptions);         if (!$conn) {            die("Error connection: ".sqlsrv_errors());        }
 echo "DB Server: Connected!";
 ?>

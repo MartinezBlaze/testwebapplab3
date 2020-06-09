61105b644e261c54358a07e6659afba413a1a781
Yoseph Martin Christian <14261062+MartinezBlaze@users.noreply.github.com>
2020-06-09 11:44:03 +0700

    Update viewrestaurant.php

--- viewrestaurant.php
+++ viewrestaurant.php
@@ -5,10 +5,21 @@
     "Uid" => "apuadmin",
     "PWD" => "Martin070899?");
 
-//Establishes the connection        
-$conn = sqlsrv_connect($serverName, $connectionOptions);
-if (!$conn) {
-  die("Error connection: ".sqlsrv_errors());
-}
-echo "DB Server: Connected!";
+  //Establishes the connection        
+  $conn = sqlsrv_connect($serverName, $connectionOptions);
+  if (!$conn) {
+    die("Error connection: ".sqlsrv_errors());
+  }
+  echo "DB Server: Connected!";
+
+  while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC))
+  {
+    echo "<tr>";
+    echo "<td>". $row['restaurant_id'] . "</td>";
+    echo "<td>". $row['restaurant_name'] ."</td>";
+    echo "<td>". $row['restaurant_address'] . "</td>";
+    echo "<td>". $row['restaurant_phone'] . "</td>";
+    echo "</tr>";
+  }
+  sqlsrv_free_stmt($getResults); 
 ?>
